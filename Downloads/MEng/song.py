38a0da8cb4b073d967ee97b287676e3323f23e91
Beau Si <bs655@cornell.edu>
2020-04-29 18:33:36 -0500

    song

---
+++ Downloads/MEng/song.py
@@ -0,0 +1,12 @@
+#setting up serial connection to Raspberry Pi for the Roomba
+ser = serial.Serial(port = '/dev/ttyUSB0',baudrate=115200)
+ser.flushOutput()
+START = '\x80'
+ser.write(START)
+SAFEMODE = '\x83' 
+ser.write(SAFEMODE)
+
+#SING A SONG
+ser.write("\x8C\x00\x01\x45\xFF")
+
+ser.close()
